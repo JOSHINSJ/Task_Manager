ed13871910e213cd7b75cace8fc21b9fd9f5af6d
JOSHINSJ <103443492+JOSHINSJ@users.noreply.github.com>
2022-04-11 22:16:31 +0530

    Add files via upload

---
+++ side_bar.php
@@ -0,0 +1,197 @@
+ <!-- Main Sidebar Container -->
+ <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="#" class="brand-link">
+      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+           style="opacity: .8">
+      <span class="brand-text font-weight-light">Tasks</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="image/niranjan.JPG" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Niranjan</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item has-treeview menu-open">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+                <i class="right fas fa-angle-left"></i>
+              </p>
+            </a>
+            <ul class="nav-treeview">
+              <li class="nav-item">
+                <a href="./sales_2.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Sales</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="./purchase.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Purchase</p>
+                </a>
+              </li>
+            </ul>
+          </li>
+  <!-- Sales        -->
+          <li class="nav-header">Sales</li>
+
+          <li class="nav-item" >
+            <a href="./enquiry.php" class="nav-link">
+              <i class="nav-icon fas fa fa-clipboard"></i>
+              <p>
+                Enquiry
+                <!-- <span class="right badge badge-danger">New</span> -->
+              </p>
+            </a>
+          </li>
+          <li class="nav-item" >
+            <a href="./enquiry_review.php" class="nav-link">
+              <i class="nav-icon fas fa-copy"></i>
+              <p>
+                Enquiry Review
+              </p>
+            </a>
+          </li>
+          <li class="nav-item" >
+            <a href="./quotation.php" class="nav-link">
+              <i class="nav-icon fas fa-book"></i>
+              <p>
+                Quotation
+              </p>
+            </a>
+          </li>
+          <li class="nav-item" >
+            <a href="./order_acceptance.php" class="nav-link">
+              <i class="nav-icon fas fa-edit"></i>
+              <p>
+                Order Acceptance
+              </p>
+            </a>
+          </li>
+
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-table"></i>
+              <p>
+                Reports
+                <i class="fas fa-angle-left right"></i>
+                <span class="badge badge-info right">3</span>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="./reports.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Current</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Weekly</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Monthly</p>
+                </a>
+               
+              </li>
+              </ul>
+
+  <!-- Purchases       -->
+                <li class="nav-header">Purchase</li>
+
+                <li class="nav-item" >
+                <a href="./pa_request.php" class="nav-link">
+                    <i class="nav-icon fas fa fa-clipboard"></i>
+                    <p>
+                    Reqeuest
+                    <!-- <span class="right badge badge-danger">New</span> -->
+                    </p>
+                </a>
+                </li>
+                <li class="nav-item" >
+                <a href="./pa_direct_issue.php" class="nav-link">
+                    <i class="nav-icon fas fa-copy"></i>
+                    <p>
+                    Direct Issue
+                    </p>
+                </a>
+                </li>
+                <li class="nav-item" >
+                <a href="./pa_po.php" class="nav-link">
+                    <i class="nav-icon fas fa-book"></i>
+                    <p>
+                    Purchase Order
+                    </p>
+                </a>
+                </li>
+                <li class="nav-item" >
+                <a href="./pa_grn.php" class="nav-link">
+                    <i class="nav-icon fas fa-edit"></i>
+                    <p>
+                    GRN
+                    </p>
+                </a>
+                </li>
+                <li class="nav-item" >
+                <a href="./pa_stock.php" class="nav-link">
+                    <i class="nav-icon fas fa-edit"></i>
+                    <p>
+                    Stock
+                    </p>
+                </a>
+                </li>
+
+                <li class="nav-item has-treeview">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-table"></i>
+                    <p>
+                    Reports
+                    <i class="fas fa-angle-left right"></i>
+                    <span class="badge badge-info right">3</span>
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+                    <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Current</p>
+                    </a>
+                    </li>
+                    <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Weekly</p>
+                    </a>
+                    </li>
+                    <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Monthly</p>
+                    </a>
+                    
+                    </li> -->
+                    </ul>        
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
